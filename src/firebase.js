51355e3fb2f9417582320c6bd51fa451a007d84b
keota.mar <keota.mar@gmail.com>
2026-01-06 16:09:40 +0000

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,25 @@
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth'; 
 
-// 🟢 ຢ່າລືມເອົາ Config ຂອງເຈົ້າມາໃສ່ບ່ອນນີ້ເດີ້ (ໂຕທີ່ເປັນ AIzaSy...)
+// 🟢 ຢ່າລືມເອົາ Config ຂອງເຈົ້າມາໃສ່ຄືເກົ່າເດີ້
 const firebaseConfig = {
-    apiKey: "AIzaSyAq2zXT4AeLbbDre8lEh5KgIvq5xtoj1-o",
-    authDomain: "studio-7834307833-10afa.firebaseapp.com",
-    databaseURL: "https://studio-7834307833-10afa-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "studio-7834307833-10afa",
-    storageBucket: "studio-7834307833-10afa.firebasestorage.app",
-    messagingSenderId: "1085134944350",
-    appId: "1:1085134944350:web:cdd4c563a7891c176d9ccf"
-  };
-  
+  apiKey: "AIzaSyAq2zXT4AeLbbDre8lEh5KgIvq5xtoj1-o",
+  authDomain: "studio-7834307833-10afa.firebaseapp.com",
+  databaseURL: "https://studio-7834307833-10afa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-7834307833-10afa",
+  storageBucket: "studio-7834307833-10afa.firebasestorage.app",
+  messagingSenderId: "1085134944350",
+  appId: "1:1085134944350:web:cdd4c563a7891c176d9ccf"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth with Persistence (ເພື່ອໃຫ້ມັນຈື່ການ Login ໄວ້)
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
+// Auth ແບບທຳມະດາ (ບໍ່ຕ້ອງຈື່ Session)
+const auth = getAuth(app);
 
 // Initialize Database
 const db = getDatabase(app);
 
-export { auth, db };
+export { db, auth };

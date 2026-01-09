682c53c3d69f1b5ef01e393841396a84e1340560
keota.mar <keota.mar@gmail.com>
2026-01-09 15:55:42 +0000

    commit

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAq2zXT4AeLbbDre8lEh5KgIvq5xtoj1-o",
+    authDomain: "studio-7834307833-10afa.firebaseapp.com",
+    databaseURL: "https://studio-7834307833-10afa-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "studio-7834307833-10afa",
+    storageBucket: "studio-7834307833-10afa.firebasestorage.app",
+    messagingSenderId: "1085134944350",
+    appId: "1:1085134944350:web:cdd4c563a7891c176d9ccf"
+  };
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);

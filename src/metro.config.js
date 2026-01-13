5dcd1319dbbd0c6fbdf4134f0cd6f6204c4e5f4e
keota.mar <keota.mar@gmail.com>
2026-01-13 09:14:58 +0000

    commit

--- src/metro.config.js
+++ src/metro.config.js
@@ -1,9 +1,8 @@
-// metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// ເພີ່ມ cjs ເພື່ອໃຫ້ Metro ອ່ານໄຟລ໌ຂອງ Firebase v11 ໄດ້ຖືກຕ້ອງ
+// ເພີ່ມ cjs ເຂົ້າໄປເພື່ອໃຫ້ Metro ອ່ານໄຟລ໌ຂອງ Firebase v11 ໄດ້ຖືກຕ້ອງ
 config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
